2410e7e33f525c729dfa86ce9086e7edbc6b4050
PnutButtrCrepes <77402483+PnutButtrCrepes@users.noreply.github.com>
2022-04-23 01:43:07 -0400

    Update player directly
    Git bash not working.

--- ComputerNetworksProject/Assets/NathanPlayground/Scripts/Powerup.cs
+++ ComputerNetworksProject/Assets/NathanPlayground/Scripts/Powerup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Placeholder : MonoBehaviour
+public class Powerup : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class Placeholder : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
     }
 }
